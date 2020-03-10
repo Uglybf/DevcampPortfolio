9c3c6b333834d9e867c5533c9b443ea885a3f6a8
Uglybf <Uglybf@users.noreply.github.com>
2020-03-10 13:28:38 +0100

    Implemented validation for Blog, Portfolio and Skill

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,4 @@
 class Portfolio < ApplicationRecord
+
+  validates_presece_of :title, :body, :main_image, :thumb_image
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+
+  validates_presece_of :title, :percent_utiliced
 end
