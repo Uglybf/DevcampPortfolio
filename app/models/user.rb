3a0205075a3f0601b69f89d06e6421f37989adb3
Uglybf <Uglybf@users.noreply.github.com>
2020-03-20 17:49:09 +0100

    bugfix: removed .oops method from users view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,20 @@ class User < ApplicationRecord
   validates_presence_of :name
 
   def first_name
-    self.name.split.first
+      self.name.split.first
   end
 
   def last_name
     self.name.split.last
+  end
+end
+
+
+def get_facebook_messages
+  begin
+    contacts_fb.oops
+    @messages = retrieves_messages
+  resucue IOError => e
+    flash[:error] = "Error accurred contacting Facebook: #{e}"
   end
 end
