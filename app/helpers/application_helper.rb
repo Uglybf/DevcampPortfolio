e93bf5d1115c5d0b448d5ac46440a3dfb39a4740
Uglybf <Uglybf@users.noreply.github.com>
2020-04-06 14:59:28 +0200

    Refacured alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -93,4 +93,25 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Please pay attention! Portfolio", sticky: false, time: 10000)
+  end
+
+
+
+
+
+
+
+
+
 end
